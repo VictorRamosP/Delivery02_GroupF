4e944ba8b63ccfaae265966d218657573eb2471f
izan302 <izan302.yt@gmail.com>
2025-02-12 18:56:12 +0100

    Cosas a medias UwU

---
+++ Assets/FollowGoal.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FollowGoal : MonoBehaviour
+{
+    public Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
--- Assets/Scripts/Orc/PatrolBehaviour.cs
+++ Assets/Scripts/Orc/PatrolBehaviour.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
+using UnityEngine;
 
 public class PatrolBehaviour : StateMachineBehaviour
 {
     public float StayTime;
     public float VisionRange;
     public float Speed = 2f;
-
+    public float RaycastDistance;
+    public LayerMask WhatIsWall;
     private float _timer;
     private Transform _player;
     private Vector2 _target;
@@ -18,9 +20,29 @@ public class PatrolBehaviour : StateMachineBehaviour
         _timer = 0.0f;
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _rb = animator.GetComponent<Rigidbody2D>(); // Obtener el Rigidbody2D
+        
+        var transform = animator.transform;
+        Vector2 newDirection = new Vector2(Random.Range(-1f, 1f) * 4, Random.Range(-1f, 1f) * 4);
+        if (!EdgeDetected(transform.position, transform.up)) {
+            if (!EdgeDetected(transform.position, transform.right)) {
+                newDirection = Vector2.up + Vector2.right;
+                UnityEngine.Debug.Log("UR");                     
+            }else if (!EdgeDetected(transform.position, -transform.right)) {
+                newDirection = Vector2.up + Vector2.left;
+                UnityEngine.Debug.Log("UL");                          
+            }                         
+        }else if (!EdgeDetected(transform.position, -transform.up)) {
+            if (!EdgeDetected(transform.position, transform.right)) {
+                newDirection = Vector2.down + Vector2.right;
+                UnityEngine.Debug.Log("DR");                        
+            }else if (!EdgeDetected(transform.position, -transform.right)) {
+                newDirection = Vector2.down + Vector2.left;    
+                UnityEngine.Debug.Log("DL");                    
+            }                       
+        }
 
         Vector2 startPos = animator.transform.position;
-        _target = startPos + new Vector2(Random.Range(-1f, 1f) * 4, Random.Range(-1f, 1f) * 4);
+        _target = startPos + newDirection;
     }
 
     // OnStateUpdate is called on each Update frame between
@@ -52,5 +74,10 @@ public class PatrolBehaviour : StateMachineBehaviour
     {
         var dist = Vector3.Distance(transform.position, _player.position);
         return (dist < VisionRange);
+    }
+    private bool EdgeDetected(Vector3 pos, Vector3 _direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(pos, _direction, RaycastDistance, WhatIsWall);
+        return (hit.collider == null);
     }
 }
