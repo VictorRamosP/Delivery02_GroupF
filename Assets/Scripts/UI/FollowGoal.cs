af8de6afca2f2fa00a1f61e6be7cdeb6f3965b45
izan302 <izan302.yt@gmail.com>
2025-02-12 21:46:22 +0100

    Goal Indicator

--- Assets/Scripts/UI/FollowGoal.cs
+++ Assets/Scripts/UI/FollowGoal.cs
@@ -14,6 +14,7 @@ public class FollowGoal : MonoBehaviour
     {
         if (target != null)
         {
+            Debug.Log(target);
             Vector3 direction = (target.position - transform.position).normalized;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, 0, angle);
